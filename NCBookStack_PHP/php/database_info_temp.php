bf61d00b80ccea31c60f55ac1f6849fc151c4923
john <bookerboker@gmail.com>
2014-02-18 23:11:18 -0500

    Changed php file storing database access information and how processRequests.php gets database access information

---
+++ NCBookStack_PHP/php/database_info_temp.php
@@ -0,0 +1,13 @@
+<?php
+# 1) Make a copy of this file, the copy to hold private database information and 
+#  the original to serve as a template for anyone who forks this code.
+# 2) Fill in constants with relevent information for use with database access
+# 3) Then rename file to database_info.php
+# 4) In netbeans, right click on file then go to git->Ignore->Ignore
+#  so file is not uploaded to github with private information
+define("HOST", "");
+define("USERNAME", "");
+define("PASSWORD", "");
+define("DATABASE_NAME", "");
+define("TABLE_NAME", "");
+?>
--- NCBookStack_PHP/php/userpass.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("DATABASE_USERNAME", "");
-define("DATABASE_PS", "");
-define("DB_NAME", "book_slice1");
-?>
